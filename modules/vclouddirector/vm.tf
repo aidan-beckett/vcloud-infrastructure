b4d9dd5a735d6cd9f37b6a2f8d608777f500680f
Aidan Beckett <aidan.beckett@synextra.co.uk>
2022-01-18 16:43:02 +0000

    Update vm.tf

--- modules/vclouddirector/vm.tf
+++ modules/vclouddirector/vm.tf
@@ -1,19 +1,23 @@
 resource "vcd_vm" "vms" {
-  for_each    = {for vm_config in var.vm_configs: vm_config.vm_name => vm_config}
+  for_each      = {for vm_config in var.vm_configs: vm_config.vm_name => vm_config}
 
-  org         = var.create_new_org? vcd_org.customer_org[0].name : var.org_name
-  vdc         = vcd_org_vdc.customer_vdc.name
+  org           = var.create_new_org? vcd_org.customer_org[0].name : var.org_name
+  vdc           = vcd_org_vdc.customer_vdc.name
   sizing_policy_id = vcd_org_vdc.customer_vdc.default_vm_sizing_policy_id
   catalog_name  = "OS Library"
   template_name = each.value.template_name
-  name        = each.value.vm_name
+  name          = each.value.vm_name
+
+  cpus          = each.value.cpus
+  cpu_cores     = each.value.cpus
+  memory        = each.value.memory
 
   network {
 
-    name      = var.network_name
-    type      = "org"
+    name        = var.network_name
+    type        = "org"
     ip_allocation_mode = "MANUAL"
-    ip        = each.value.vm_ip
+    ip          = each.value.vm_ip
 
   }
 
