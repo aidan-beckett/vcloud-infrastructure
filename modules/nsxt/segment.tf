5e2dd85de9fa25907bb262bbc29eaeb5791706f5
Janet Li <janet.li@synextra.co.uk>
2022-01-27 15:45:38 +0000

    remove optional files

--- modules/nsxt/segment.tf
+++ modules/nsxt/segment.tf
@@ -1,7 +1,6 @@
 resource "nsxt_policy_vlan_segment" "man_vlan_seg" {
   count               = var.man_segment_display_name != "" ? 1 : 0 
   display_name        = var.man_segment_display_name
-  description         = "Terraform provisioned VLAN Segment Manchester"
   transport_zone_path = "/infra/sites/default/enforcement-points/default/transport-zones/2ab7b77f-c4d4-483e-9686-09b0ecd99ba3"
   vlan_ids = var.man_segment_vlan_ids
 }
@@ -9,14 +8,12 @@ resource "nsxt_policy_vlan_segment" "man_vlan_seg" {
 resource "nsxt_policy_vlan_segment" "lds_vlan_seg" {
   count               = var.lds_segment_display_name != "" ? 1 : 0 
   display_name        = var.lds_segment_display_name
-  description         = "Terraform provisioned VLAN Segment Leeds"
   transport_zone_path = "/infra/sites/default/enforcement-points/default/transport-zones/0b85d17b-4160-4083-b548-1d596b62b5ec"
   vlan_ids = var.lds_segment_vlan_ids
 }
 
 resource "nsxt_policy_segment" "overlay_seg" {
   display_name        = var.overlay_segment_name
-  description         = "Terraform provisioned Overlay Segment"
   connectivity_path   = nsxt_policy_tier1_gateway.tier1_gw.path
   transport_zone_path = "/infra/sites/default/enforcement-points/default/transport-zones/839090f3-a229-4753-9a20-94c51d6da2bb"
 
