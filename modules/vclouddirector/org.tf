bed575ac58faeb01baf63382cce67d088a190bec
Aidan Beckett <aidan.beckett@synextra.co.uk>
2022-02-24 10:57:41 +0000

    Update org.tf

--- modules/vclouddirector/org.tf
+++ modules/vclouddirector/org.tf
@@ -3,12 +3,12 @@ resource "vcd_org" "customer_org" {
   name             = var.org_name
   full_name        = var.org_full_name
   description      = "Terraform provisioned Org"
-  delete_recursive = false
-  delete_force     = false
+  delete_recursive = true
+  delete_force     = true
 
   vapp_lease {
     maximum_runtime_lease_in_sec          = 0
-    power_off_on_runtime_lease_expiration = true
+    power_off_on_runtime_lease_expiration = false
     maximum_storage_lease_in_sec          = 0 # never expires
     delete_on_storage_lease_expiration    = false
   }
